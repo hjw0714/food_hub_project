d104d234e808995d5b7919ea4b1fe935e82bff88
kwonsb96 <kwonsb96@gmail.com>
2025-02-11 09:41:29 +0900

    좋아요 부분 수정

--- food_hub_project/src/main/java/com/application/foodhub/post/PostController.java
+++ food_hub_project/src/main/java/com/application/foodhub/post/PostController.java
@@ -3,6 +3,7 @@ package com.application.foodhub.post;
 import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,12 +11,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -25,6 +28,8 @@ import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import com.application.foodhub.comment.CommentService;
 import com.application.foodhub.fileUpload.FileUploadDTO;
 import com.application.foodhub.fileUpload.FileUploadService;
+import com.application.foodhub.postLike.PostLikeDTO;
+import com.application.foodhub.postLike.PostLikeService;
 
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpSession;
@@ -43,7 +48,8 @@ public class PostController {
 	private CommentService commentService;
 
 	@Autowired
-	   private PostLikeService postLikeService; 
+	private PostLikeService postLikeService;
+
 	/**
 	 * 통합 게시판 목록 조회
 	 */
@@ -306,30 +312,28 @@ public class PostController {
 		}
 		return jsScript;
 	}
-	
+
 	@GetMapping("/notification")
 	public String notificationPage(Model model) {
-	    List<Map<String, Object>> postList = postService.getPostList(15, 0);
+		List<Map<String, Object>> postList = postService.getPostList(15, 0);
 
-	    if (postList == null) {
-	        postList = new ArrayList<>(); // 빈 리스트 반환
-	    }
+		if (postList == null) {
+			postList = new ArrayList<>(); // 빈 리스트 반환
+		}
 
-	    model.addAttribute("postListMap", postList);
-	    return "foodhub/post/notification";
+		model.addAttribute("postListMap", postList);
+		return "foodhub/post/notification";
 	}
-	
+
 	@PostMapping("/postLike")
-	   public String postLike(@RequestBody PostLikeDTO postLikeDTO) {
-	      
-	      long postId = postLikeDTO.getPostId();
-	      String userId = postLikeDTO.getUserId();
-	      
-	      postLikeService.togglePostLike(postId, userId);
-	      
-	      //System.out.println("postid : " + postId);
-	      //System.out.println("userId : " + userId);
-	      
-	      return "";
-	   }
+	public int postLike(@RequestBody PostLikeDTO postLikeDTO) {
+		long postId = postLikeDTO.getPostId();
+		String userId = postLikeDTO.getUserId();
+
+		postLikeService.togglePostLike(postId, userId);
+		int likeCount = postLikeService.getPostLikeCount(postId);
+
+
+		return likeCount;
+	}
 }
